dbf9a56146c2691e71c0cb8616c997770c0b1615
beerbits <rodma.info@gmail.com>
2014-06-09 17:46:25 -0300

    Criação do código gerador de número aleatório
    Contém erros de lógica, deverá ser corrigo para prosseguir com o
    projeto.

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+
+int gerNum_Aleatorio(){
+    int x, i, j, vet[24];
+    srand(time(NULL));
+
+    for(i=0;i<25;i++){
+
+        x = 1 + ( rand() % 12 );
+        for(j=0;j<25;j++){
+            if(vet[j]!=x){
+                vet[j] = x;
+            }
+        }
+
+    }
+    return x;
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    int matriz[6][4], i, j;
+
+    for(i=0; i<6;i++){
+        for(j=0;j<4;j++){
+            matriz[i][j]= gerNum_Aleatorio();
+        }
+    }
+
+
     return 0;
 }
